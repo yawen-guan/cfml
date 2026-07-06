f0134728102e7fb92caa960ee5e26548f968725e
Yawen Guan <yawen.guan@proton.me>
2026-07-06 14:50:13 +0200

    Replace continuation-style WPgen combinators

--- lib/coq/WPLifted.v
+++ lib/coq/WPLifted.v
@@ -354,14 +354,6 @@ Proof using. introv HH M1. apply MkStruct_erase. xchanges* HH. Qed.
 (* ** Definition of CF blocks for the internal generator -- DEPRECATED? *)
 
 
-Definition Wpgen_let_trm_cont (F1:Formula) A1 {EA1:Enc A1} (F2of:A1->Formula) : Formula :=
-  MkStruct (fun A (EA:Enc A) Q =>
-    ^F1 (fun (X:A1) => ^(F2of X) Q)).
-
-Definition Wpgen_seq_cont (F1 F2:Formula) : Formula :=
-  MkStruct (fun A (EA:Enc A) Q =>
-    ^F1 (fun (X:unit) => ^F2 Q)).
-
 Definition Wpgen_val_unlifted (v:val) : Formula :=
   MkStruct (fun A (EA:Enc A) Q => Post Q v).
 
@@ -382,12 +374,12 @@ Definition Wpaux_getval Wpgen (E:ctx) (t1:trm) (F2of:val->Formula) : Formula :=
 
 Definition Wpaux_getval_typed Wpgen (E:ctx) (t1:trm) `{EA1:Enc A1} (F2of:A1->Formula) : Formula :=
   match t1 with
-  | trm_val v => Wptag (Wpgen_let_trm_cont (Wptag (Wpgen_val_unlifted v)) F2of)
+  | trm_val v => Wptag (Wpgen_let_trm (Wptag (Wpgen_val_unlifted v)) F2of)
   | trm_var x => match Ctx.lookup x E with
-                 | Some v => Wptag (Wpgen_let_trm_cont (Wptag (Wpgen_val_unlifted v)) F2of)
+                 | Some v => Wptag (Wpgen_let_trm (Wptag (Wpgen_val_unlifted v)) F2of)
                  | None => Wptag (Wpgen_fail)
                  end
-  | _ => Wptag (Wpgen_let_trm_cont (Wpgen E t1) F2of)
+  | _ => Wptag (Wpgen_let_trm (Wpgen E t1) F2of)
   end.
 
 Definition Wpaux_constr Wpgen (E:ctx) (id:idconstr) : list val -> list trm -> Formula :=
@@ -415,19 +407,19 @@ Definition Wpaux_var (E:ctx) (x:var) : Formula :=
   end.
 
 Definition Wpaux_if_trm (F0 F1 F2:Formula) : Formula :=
-  Wptag (Wpgen_let_trm_cont F0 (fun (b:bool) => Wptag (Wpgen_if b F1 F2))).
+  Wptag (Wpgen_let_trm F0 (fun (b:bool) => Wptag (Wpgen_if b F1 F2))).
 
 Definition Wpgen_while (F1 F2:Formula) : Formula :=
   MkStruct (Wptag (FormulaCast (fun (Q:unit->hprop) =>
     \forall (R:Formula),
-    let F := Wpaux_if_trm F1 (Wpgen_seq_cont F2 R) (Wpgen_val_unlifted val_unit) in
+    let F := Wpaux_if_trm F1 (Wpgen_seq F2 R) (Wpgen_val_unlifted val_unit) in
     \[ structural (@R unit _) /\ (forall (Q':unit->hprop), ^F Q' ==> ^R Q')] \-* (^R Q)))).
     (* --TODO: use a lifted version of structural *)
 
 Definition Wpgen_for_int (n1 n2:int) (F1:int->Formula) : Formula :=
   MkStruct (FormulaCast (fun (Q:unit->hprop) =>
     \forall (S:int->Formula),
-    let F i := If (i <= n2) then (Wptag (Wpgen_seq_cont (F1 i) (S (i+1))))
+    let F i := If (i <= n2) then (Wptag (Wpgen_seq (F1 i) (S (i+1))))
                             else (Wptag (Wpgen_val_unlifted val_unit)) in
     \[   (forall i, structural (S i unit _))
       /\ (forall i (Q':unit->hprop), ^(F i) Q' ==> ^(S i) Q')] \-* (^(S n1) Q))).
@@ -436,7 +428,7 @@ Definition Wpgen_for_int (n1 n2:int) (F1:int->Formula) : Formula :=
 Definition Wpgen_for_downto_int (n1 n2:int) (F1:int->Formula) : Formula :=
   MkStruct (FormulaCast (fun (Q:unit->hprop) =>
     \forall (S:int->Formula),
-    let F i := If (i >= n2) then (Wptag (Wpgen_seq_cont (F1 i) (S (i-1))))
+    let F i := If (i >= n2) then (Wptag (Wpgen_seq (F1 i) (S (i-1))))
                             else (Wptag (Wpgen_val_unlifted val_unit)) in
     \[   (forall i, structural (S i unit _))
       /\ (forall i (Q':unit->hprop), ^(F i) Q' ==> ^(S i) Q')] \-* (^(S n1) Q))).
@@ -478,7 +470,7 @@ Fixpoint Wpgen (E:ctx) (t:trm) : Formula :=
        Wptag (Wpgen_if b0 (aux t1) (aux t2)))
   | trm_let z t1 t2 =>
      match z with
-     | bind_anon => Wptag (Wpgen_seq_cont (aux t1) (aux t2))
+     | bind_anon => Wptag (Wpgen_seq (aux t1) (aux t2))
      | bind_var x => Wptag (Wpgen_let (aux t1) (fun A (EA:Enc A) (X:A) =>
                          Wpgen (Ctx.add x (enc X) E) t2))
      end
@@ -677,15 +669,16 @@ Proof using.
   remove_MkStruct. apply Triple_of_Wp. case_if. { applys M1. } { applys M2. }
 Qed.
 
-Lemma Wpgen_sound_seq_cont : forall (F1 F2:Formula) E t1 t2,
+Lemma Wpgen_sound_seq : forall (F1 F2:Formula) E t1 t2,
   F1 ====> Wpsubst E t1 ->
   F2 ====> Wpsubst E t2 ->
-  Wpgen_seq_cont F1 F2 ====> Wpsubst E (trm_seq t1 t2).
-Proof using.
-  introv M1 M2. intros A EA. applys qimpl_Wp_of_Triple. intros Q.
-  remove_MkStruct. simpl. applys Triple_seq.
-  { rewrite Triple_eq_himpl_Wp. applys* M1. }
-  { rewrite Triple_eq_himpl_Wp. applys* M2. }
+  Wpgen_seq F1 F2 ====> Wpsubst E (trm_seq t1 t2).
+Proof.
+  introv M1 M2. intros A EA. applys qimpl_Wp_of_Triple. intros Q. remove_MkStruct.
+  apply Triple_hexists; intros Q1. rewrite hstar_comm; apply Triple_hpure; intros M3.
+  simpl. applys Triple_seq.
+  { rewrite Triple_eq_himpl_Wp. applys M1. }
+  { rewrite Triple_eq_himpl_Wp. xchange M3. applys M2. }
 Qed.
 
 Lemma Wpgen_sound_let : forall (F1:Formula) (F2of:forall `{EA1:Enc A1},A1->Formula) E (x:var) t1 t2,
@@ -704,27 +697,30 @@ Qed.
 Lemma Wpgen_sound_let_typed : forall (F1:Formula) `{EA1:Enc A1} (F2of:A1->Formula) E (x:var) t1 t2,
   F1 ====> Wpsubst E t1 ->
   (forall (X:A1), F2of X ====> Wpsubst (Ctx.add x (enc X) E) t2) ->
-  Wpgen_let_trm_cont F1 F2of ====> Wpsubst E (trm_let x t1 t2).
+  Wpgen_let_trm F1 F2of ====> Wpsubst E (trm_let x t1 t2).
 Proof using.
   Opaque Ctx.rem.
-  introv M1 M2. intros A EA. applys qimpl_Wp_of_Triple. intros Q.
-  remove_MkStruct. xtpull. simpl. applys Triple_let EA1.
+  introv M1 M2. intros A EA. applys qimpl_Wp_of_Triple. intros Q. remove_MkStruct.
+  apply Triple_hexists; intros Q1. rewrite hstar_comm; apply Triple_hpure; intros M3.
+  xtpull. simpl. applys Triple_let EA1.
   (* --LATER: typeclass should not be resolved arbitrarily to EA if EA1 is not provided *)
   { rewrite Triple_eq_himpl_Wp. applys* M1. }
   { intros X. rewrite Triple_eq_himpl_Wp.
-    unfold Subst1. rewrite <- isubst_add_eq_subst1_isubst. applys* M2. }
+    unfold Subst1. rewrite <- isubst_add_eq_subst1_isubst. xchange M3. applys* M2. }
 Qed.
 
 Lemma Wpgen_sound_let_typed_val : forall v E (C:trm -> trm) `{EA:Enc A} (F2of:A->Formula),
   evalctx C ->
   (forall V, F2of V ====> @Wpsubst E (C ``V)) ->
-  Wpgen_let_trm_cont (Wptag (Wpgen_val_unlifted v)) F2of ====> Wp (isubst E (C v)).
+  Wpgen_let_trm (Wptag (Wpgen_val_unlifted v)) F2of ====> Wp (isubst E (C v)).
 Proof using.
-  introv HC M1. intros A1 EA1. applys qimpl_Wp_of_Triple. intros Q.
-  remove_MkStruct. applys~ Triple_isubst_evalctx EA.
+  introv HC M1; intros A1 EA1. applys qimpl_Wp_of_Triple; intros Q. remove_MkStruct.
+  apply Triple_hexists; intros Q1.
+  rewrite hstar_comm; apply Triple_hpure; intros M2.
+  applys~ Triple_isubst_evalctx EA.
   { rewrite Triple_eq_himpl_Wp. lets K: Wpgen_sound_val.
     unfold Wpgen_sound in K. simpl in K. xchange K. }
-  { intros V. rewrite Triple_eq_himpl_Wp. xchange M1. }
+  { intros V. rewrite Triple_eq_himpl_Wp. xchange M2. xchange M1. }
 Qed.
 
 Lemma Wpgen_sound_getval_typed : forall E C t1 `{EA:Enc A} (F2of:A->Formula),
@@ -742,11 +738,13 @@ Proof using.
     { intros v Ev. rewrites~ (>> isubst_evalctx_trm_var Ev).
       apply Triple_of_Wp. applys~ Wpgen_sound_let_typed_val. }
     { introv N. remove_MkStruct. xtpull. intros; false. } }
-  asserts_rewrite (Wpaux_getval_typed Wpgen E t1 (@F2of) = Wpgen_let_trm_cont (Wpgen E t1) F2of).
+  asserts_rewrite (Wpaux_getval_typed Wpgen E t1 (@F2of) = Wpgen_let_trm (Wpgen E t1) F2of).
   { destruct t1; auto. { false C2. hnfs*. } }
-  remove_MkStruct. applys~ Triple_isubst_evalctx EA.
+  remove_MkStruct.
+  apply Triple_hexists; intros Q1. rewrite hstar_comm; apply Triple_hpure; intros M3.
+  applys~ Triple_isubst_evalctx EA.
   { apply Triple_of_Wp. applys M1. }
-  { intros V. apply Triple_of_Wp. applys M2. }
+  { intros V. apply Triple_of_Wp. xchange M3. applys M2. }
 Qed.
 
 Lemma Wpgen_sound_if_trm : forall (F0 F1 F2:Formula) E t0 t1 t2,
@@ -755,10 +753,11 @@ Lemma Wpgen_sound_if_trm : forall (F0 F1 F2:Formula) E t0 t1 t2,
   F2 ====> (Wpsubst E t2) ->
   Wpaux_if_trm F0 F1 F2 ====> Wpsubst E (trm_if t0 t1 t2).
 Proof using.
-  introv M0 M1 M2. intros A EA. applys qimpl_Wp_of_Triple. intros Q.
-  remove_MkStruct. xtpull. simpl. applys Triple_if_trm.
+  introv M0 M1 M2. intros A EA. applys qimpl_Wp_of_Triple. intros Q. remove_MkStruct.
+  apply Triple_hexists; intros Q1. rewrite hstar_comm; apply Triple_hpure; intros M3.
+  xtpull. simpl. applys Triple_if_trm.
   { rewrite Triple_eq_himpl_Wp. applys* M0. }
-  { intros b. apply Triple_of_Wp. applys* Wpgen_sound_if_bool'. }
+  { intros b. apply Triple_of_Wp. xchange M3. applys* Wpgen_sound_if_bool'. }
 Qed.
 
 Lemma Wpgen_sound_if : forall t1 t2 t3,
@@ -813,7 +812,7 @@ Proof using.
          trm_if (isubst E t1) (trm_seq (isubst E t2) (trm_while (isubst E t1) (isubst E t2))) val_unit
        = isubst E (trm_if t1 (trm_seq t2 (trm_while t1 t2)) val_unit)).
       rewrite Triple_eq_himpl_Wp. applys~ Wpgen_sound_if_trm.
-      { applys~ Wpgen_sound_seq_cont. }
+      { applys~ Wpgen_sound_seq. }
       { intros A1 EA1 Q''. applys Wpgen_sound_val. } } }
   { rewrite~ @Triple_eq_himpl_Wp. }
 Qed.
@@ -837,7 +836,7 @@ Proof using. Opaque Ctx.add Ctx.rem.
         asserts_rewrite (trm_seq (isubst (Ctx.add x (``i) E) t1) (trm_for x (i + 1)%I n2 (isubst (Ctx.rem x E) t1))
           = (isubst (Ctx.add x (``i) E) (trm_seq t1 (trm_for x (i + 1)%I n2 t1)))).
         { simpl. rewrite Ctx.rem_anon, Ctx.rem_add_same. auto. }
-        applys Wpgen_sound_seq_cont.
+        applys Wpgen_sound_seq.
         { applys* M. }
         { unfold S. unfold Wpsubst. simpl. rewrite~ Ctx.rem_add_same. } }
       { applys Wpgen_sound_val E. } } }
@@ -914,7 +913,7 @@ Proof using.
   { applys~ Wpgen_sound_constr. }
   { applys* Wpgen_sound_if. }
   { destruct b as [|x].
-    { applys* Wpgen_sound_seq_cont. }
+    { applys* Wpgen_sound_seq. }
     { applys* Wpgen_sound_let. } }
   { applys* Wpgen_sound_apps. }
   { applys* Wpgen_sound_while. }
